aad5d6a2902dae60470494b80ee087e8328570ff
rmShoeb <rmshoebne14@gmail.com>
2021-05-08 23:08:37 +0600

    created form classes

---
+++ Python/web-app-with-Flask/sample-blog/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=5, max=15)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=5, max=30)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password'), Length(min=5, max=30)])
+    submit = SubmitField('Sign up')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=5, max=15)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=5, max=30)])
+    remmber = BooleanField('Remember Me')
+    submit = SubmitField('Sign in')
